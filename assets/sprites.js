9bb1b84dcc780217ec237d31ec3c3575cde8ac4d
aecasale <aecasale326@gmail.com>
2013-11-07 15:03:13 -0500

    rewrote program

---
+++ assets/sprites.js
@@ -0,0 +1,50 @@
+{
+"frames": {
+	"slice_01.png": {
+		"frame": {"x":0, "y":0, "w":491, "h":198},
+		"spriteSourceSize": {"x":0,"y":0,"w":491,"h":198},
+		"sourceSize": {"w":491,"h":198}
+	},
+	"slice_02.png": {
+		"frame": {"x":0, "y":609, "w":408, "h":196},
+		"spriteSourceSize": {"x":0,"y":0,"w":408,"h":196},
+		"sourceSize": {"w":408,"h":196}
+	},
+	"slice_03.png": {
+		"frame": {"x":0, "y":806, "w":403, "h":204},
+		"spriteSourceSize": {"x":0,"y":0,"w":403,"h":204},
+		"sourceSize": {"w":403,"h":204}
+	},
+	"slice_04.png": {
+		"frame": {"x":440, "y":415, "w":381, "h":191},
+		"spriteSourceSize": {"x":0,"y":0,"w":381,"h":191},
+		"sourceSize": {"w":381,"h":191}
+	},
+	"slice_05.png": {
+		"frame": {"x":478, "y":199, "w":390, "h":215},
+		"spriteSourceSize": {"x":0,"y":0,"w":390,"h":215},
+		"sourceSize": {"w":390,"h":215}
+	},
+	"slice_06.png": {
+		"frame": {"x":409, "y":609, "w":400, "h":220},
+		"spriteSourceSize": {"x":0,"y":0,"w":400,"h":220},
+		"sourceSize": {"w":400,"h":220}
+	},
+	"slice_07.png": {
+		"frame": {"x":0, "y":402, "w":439, "h":206},
+		"spriteSourceSize": {"x":0,"y":0,"w":439,"h":206},
+		"sourceSize": {"w":439,"h":206}
+	},
+	"slice_08.png": {
+		"frame": {"x":0, "y":199, "w":477, "h":202},
+		"spriteSourceSize": {"x":0,"y":0,"w":477,"h":202},
+		"sourceSize": {"w":477,"h":202}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 869, "h": 1011},
+	"scale": "1"
+}
+}
--- js/game.js
+++ js/game.js
@@ -1,79 +1,103 @@
 var game = new Phaser.Game(
-                    800, 600, 
-                    Phaser.AUTO, '', 
-                    { 
-                        preload: preload, 
-                        create: create, 
-                        update: update 
-                    }
-            );
-
-//game variables
+        800, 600,
+        Phaser.CANVAS, '',
+        {
+            preload: preload,
+            create: create,
+            update: update
+        }
+        );
 var sprite, group, txtScore;
 
-//load in game assets
 function preload() {
-//    game.load.atlasJSONHash('inch', 
-//                            'assets/inchy.png', 
-///                            'assets/inchy.json')
-    game.load.atlasJSONHash('inch',
-                            'assets/sprites.png',
+    game.load.atlasJSONHash('cheetah',
+                            'assets/sheet.png',
                             'assets/sprites.js');
-    game.load.image("fish", "assets/fish.png")
-}
-
-//setup game entities
-function create() {
-    //protagonist
-    sprite = game.add.sprite(0, 0, 'inch');
-    sprite.animations.add('idle');
-    sprite.animations.play('idle', 60, true);
-    sprite.anchor.setTo(.5, 0); //center flip area
-    sprite.body.collideWorldBounds = true;
-    
-    //add text
-     var style = { font: "30px Arial", fill: "#FFFF00", fontWeight: "bold", align: "center" };
-    txtScore = game.add.text(0, 0, "0", style);
-    
-    group = game.add.group();
-    //stuff
-    for(var i = 0; i < 3; i++) {
-        var ball = group.create(i*100,0, "fish");
-        ball.acceleration.y = 100;
-        ball.body.collideWorldBounds = true;
+   game.load.image('fish',
+                   'assets/fish.png');
+   game.load.image('background',
+                   'assets/BG.png');
+                   }
+   function create () {
+       background = game.add.tileSprite(0, 0, 800, 600, "background");
+       
+       //cheetah
+       sprite = game.add.sprite(0, 380, 'cheetah');
+       sprite.animations.add('idle');
+       sprite.animations.play('idle', 20, true);
+       sprite.anchor.setTo(.5, 0);
+       sprite.body.collideWorldBounds = true;
+       
+       //text
+       var style = { font: "30px Arial", fill: "#FFFF00", 
+           fontWeight: "bold", align: "center" };
+       txtScore = game.add.text(0, 0, "0", style);
+       
+       //dead fish
+       group = game.add.group();
+       
+       var i = 0;
+       //deadfish();
+       
+       //function deadfish() {
+           for (i; i < 1; i++) {
+           var x = 1 + Math.floor(Math.random() * 799);
+        var a = 1 + Math.floor(Math.random() * 50);
+        console.log('x =' + x);
+        console.log('a =' + a);
+        var deadfish = group.create(x, 0, 'fish');
+        deadfish.body.acceleration.y = a;
+        deadfish.body.collideWorldBounds = true;
+       
     }
+    //}
+     
 }
 
-//game logic, ~30 fps
 function update() {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    {
+        //moves the sprite left
+        sprite.x -= 4;
+        //faces left
+        sprite.scale.x = -1;
+    }
     
-    //add one to score
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    {
+        //moves the sprite right
+        sprite.x += 4;
+        //faces right
+        sprite.scale.x = 1;
+    }
+    
+    //check for collision with the dead fish
+    game.physics.collide(
+            sprite,
+            group,
+            collisionHandler,
+            null,
+            this
+            );
+    
+}
+
+function collisionHandler(cheetah, fish) {
+    fish.kill();
     var score = txtScore.text;
     score ++;
     txtScore.text = score.toString();
+    fishRespawn();
     
-    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-    {
-        sprite.x -= 4; //move left
-        sprite.scale.x = -1; //face left
-    }
-    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-    {
-        sprite.x += 4; //move right
-        sprite.scale.x = 1; //face right
-    }
-    
-    //check for collision with fish
-    game.physics.collide(
-         sprite, 
-         group, 
-         collisionHandler, 
-         null, 
-         this
-    );
-    
-} //end update function
-
-function collisionHandler(protagonist, fish) {
-    fish.kill();   
 }
+
+function fishRespawn() {
+    var b;
+    for(b=0; b < 2; b++) {
+        var x = 1 + Math.floor(Math.random() * 799);
+        var a = 1 + Math.floor(Math.random() * 75);
+        var fish = group.create(x, 0, "fish");
+        fish.body.acceleration.y = a;
+        fish.body.collideWorldBounds = true;
+    }
+    }
